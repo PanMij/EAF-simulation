81356555dfc688d602f3c5e42da7c782c6c10ee9
PanMij <582876465@qq.com>
2025-12-20 15:18:56 +0800

    Update the data generation script

--- data/GenData.m
+++ data/GenData.m
@@ -2,14 +2,23 @@ clear; clc;
 
 %% Parameters
 mdl = 'PlantIdentification';
-sample_per_frame = 1000;
-stop_time = 200;
+sample_per_frame = [100 300 500 800 1000];
+stop_time = 400;
 
 %% Generate data
-simIn = Simulink.SimulationInput(mdl);
-simIn = simIn.setModelParameter('StopTime', num2str(stop_time));
-out = sim(simIn, "ShowSimulationManager", "on");
+simIn(numel(sample_per_frame), 1) = Simulink.SimulationInput(mdl);
+for i = 1:numel(sample_per_frame)
+    simIn(i) = simIn(i).setModelName(mdl);
+    simIn(i) = simIn(i).setModelParameter('StopTime', num2str(stop_time));
+    simIn(i) = simIn(i).setVariable('sample_per_frame', sample_per_frame(i));
+end
+
+if isempty(gcp('nocreate'))
+    parpool(16);  % Set number of workers if necessary
+end
+simOut = parsim(simIn, 'ShowProgress', 'on', "ShowSimulationManager", "on");
+% out = sim(simIn, "ShowSimulationManager", "on");
 
 %% Save the data
-data = struct('t', out.yout{1}.Values.Time, 'y', out.yout{1}.Values.Data(:, 1), 'u', out.yout{1}.Values.Data(:, 2));
-save('data/IdData', "data");
+% data = struct('t', out.yout{1}.Values.Time, 'y', out.yout{1}.Values.Data(:, 1), 'u', out.yout{1}.Values.Data(:, 2));
+% save('data/IdData', "data");
