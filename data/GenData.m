ca3f10198bc2634e93467b80645f4d4a24491b24
PanMij <582876465@qq.com>
2026-01-03 13:26:40 +0800

    Generate data at different electrode heights

--- data/GenData.m
+++ data/GenData.m
@@ -3,15 +3,18 @@ clear; clc;
 %% Parameters
 workers = 8;
 mdl = 'PlantIdentification';
-sample_per_frame = [60:10:90];
+sample_per_frame = 10;
+L_init_ls = linspace(0.1025, 0.4187, 10);
+L_init_ls = L_init_ls(2:end-1);
 stop_time = 400;
 
 %% Generate data
-simIn(numel(sample_per_frame), 1) = Simulink.SimulationInput(mdl);
-for i = 1:numel(sample_per_frame)
+simIn(numel(L_init_ls), 1) = Simulink.SimulationInput(mdl);
+for i = 1:numel(simIn)
     simIn(i) = simIn(i).setModelName(mdl);
     simIn(i) = simIn(i).setModelParameter('StopTime', num2str(stop_time));
-    simIn(i) = simIn(i).setVariable('sample_per_frame', sample_per_frame(i));
+    simIn(i) = simIn(i).setVariable('sample_per_frame', sample_per_frame);
+    simIn(i) = simIn(i).setVariable('L_init', L_init_ls(i));
 end
 
 if isempty(gcp('nocreate'))
@@ -32,5 +35,5 @@ for i = 1:numel(simOut)
     y = simOut(i).yout{1}.Values.Data(:, 1);
     u = simOut(i).yout{1}.Values.Data(:, 2);
     data = struct('l_hy', l_hy, 't_hy', t_hy, 't', t, 'y', y, 'u', u);
-    save(sprintf("data/Id%d", sample_per_frame(i)), "data", "-v7.3");
+    save(sprintf("data/Id%.4f.mat", L_init_ls(i)), "data", "-v7.3");
 end
