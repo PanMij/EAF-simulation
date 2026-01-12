c0a15d21b97807c80f94bf8cdecff52987b5cbf3
PanMij <582876465@qq.com>
2026-01-12 09:47:06 +0800

    Add a three-phase data generation script

--- data/GenData_single_phase.m
+++ data/GenData_single_phase.m
@@ -3,6 +3,7 @@ clear; clc;
 %% Parameters
 workers = 8;
 mdl = 'PlantIdentification';
+sample_per_frame = 10;
 L_init_ls = linspace(0.1025, 0.4187, 10);
 L_init_ls = L_init_ls(2:end-1);
 
@@ -19,15 +20,17 @@ end
 simOut = parsim(simIn, "ShowSimulationManager", "on");
 
 %% Save the data
+% data = struct('t', out.yout{1}.Values.Time, 'y', out.yout{1}.Values.Data(:, 1), 'u', out.yout{1}.Values.Data(:, 2));
+% save('data/IdData', "data");
 for i = 1:numel(simOut)
     if ~isempty(simOut(i).ErrorMessage)
         continue;
     end
-    l_hy = simOut(i).logsout.getElement('l_hy').Values.Data;
-    t_hy = simOut(i).logsout.getElement('l_hy').Values.Time;
+    l_hy = simOut(i).logsout{1}.Values.Data;
+    t_hy = simOut(i).logsout{1}.Values.Time;
     t = simOut(i).yout{1}.Values.Time;
-    y = simOut(i).yout{1}.Values.Data(:, 1:3);
-    u = simOut(i).yout{1}.Values.Data(:, 4:6);
+    y = simOut(i).yout{1}.Values.Data(:, 1);
+    u = simOut(i).yout{1}.Values.Data(:, 2);
     data = struct('l_hy', l_hy, 't_hy', t_hy, 't', t, 'y', y, 'u', u);
     save(sprintf("data/Id%.4f.mat", L_init_ls(i)), "data", "-v7.3");
 end
