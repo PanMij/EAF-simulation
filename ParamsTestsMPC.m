dbd85c151ae43d0374f020818bd2f080d16da029
PanMij <582876455@qq.com>
2026-03-21 13:37:03 +0800

    Tweaks to the parameter tuning scripts

--- ParamsTestsMPC.m
+++ ParamsTestsMPC.m
@@ -19,14 +19,18 @@ mdl = 'ctrlSys_mpc';
 controller_workspace = 'MPCQP';
 
 % parameter ranges
-N2_vals = 100;
-Nu_vals = 1;
-lam1_vals = 0.2 : 0.2 : 1;
-lam2_vals = 0.2 : 0.2 : 1;
-lam3_vals = 0.2 : 0.2 : 1;
-[N2_grid, Nu_grid, lam1_grid, lam2_grid, lam3_grid] = ndgrid( ...
-    N2_vals, Nu_vals, lam1_vals, lam2_vals, lam3_vals);
-params = [N2_grid(:), Nu_grid(:), lam1_grid(:), lam2_grid(:), lam3_grid(:)];
+N2_vals = [60 80 100];
+Nu_vals = [1 10 20];
+lam1_vals = 1;
+lam2_vals = 1;
+lam3_vals = 1;
+wy1_vals = [0.1 0.5 1 5 10];
+wy2_vals = [0.1 0.5 1 5 10];
+wy3_vals = [0.1 0.5 1 5 10];
+[N2_grid, Nu_grid, lam1_grid, lam2_grid, lam3_grid, wy1_grid, wy2_grid, wy3_grid] = ndgrid( ...
+    N2_vals, Nu_vals, lam1_vals, lam2_vals, lam3_vals, wy1_vals, wy2_vals, wy3_vals);
+params = [N2_grid(:), Nu_grid(:), lam1_grid(:), lam2_grid(:), lam3_grid(:), wy1_grid(:), wy2_grid(:), wy3_grid(:)];
+params = params(params(:, 6) == params(:, 7) & params(:, 7) == params(:, 8), :);
 
 %% 2. Preallocate result storage
 nCases = size(params, 1);
@@ -52,18 +56,21 @@ for i = 1:nCases
     N2 = params(i, 1);
     Nu = params(i, 2);
     lam = params(i, 3:5);
+    wy = params(i, 6:8);
 
     % Store parameters in the SimulationInput object
+    in(i) = in(i).setVariable('CtrlChoice', "MPC_QP");
     in(i) = in(i).setVariable('N2', N2, 'Workspace', controller_workspace);
     in(i) = in(i).setVariable('Nu', Nu, 'Workspace', controller_workspace);
     in(i) = in(i).setVariable('lam', lam, 'Workspace', controller_workspace);
+    in(i) = in(i).setVariable('wy', wy, 'Workspace', controller_workspace);
     in(i) = in(i).setUserString(sprintf("sim_%d", i));
 
     % Set output locations
     in(i) = in(i).setBlockParameter( ...
-        "ctrlSys_mpc/To File step", "Filename", fullfile(save_path, "step.mat"), ...
-        "ctrlSys_mpc/To File u", "Filename", fullfile(save_path, "u.mat"), ...
-        "ctrlSys_mpc/To File r", "Filename", fullfile(save_path, "r.mat")...
+        mdl + "/To File step", "Filename", fullfile(save_path, "step.mat"), ...
+        mdl + "/To File u", "Filename", fullfile(save_path, "u.mat"), ...
+        mdl + "/To File r", "Filename", fullfile(save_path, "r.mat")...
     );
 end
 
@@ -101,6 +108,7 @@ function save_worker_output(in, save_path, idx)
     Nu = in(idx).getVariable("Nu");
     N2 = in(idx).getVariable("N2");
     lam = in(idx).getVariable("lam");
+    wy = in(idx).getVariable("wy");
 
     % Extract data
     [t_u, u] = load_log(u_path, 'u');
@@ -114,8 +122,10 @@ function save_worker_output(in, save_path, idx)
     subplot(2, 2, 1);
     plot(t_u, u);
     legend("u1", "u2", "u3");
-    title(sprintf("Controller output (Nu=%d,N2=%d,lam=[%.2f,%.2f,%.2f])", ...
-        Nu, N2, lam(1), lam(2), lam(3)));
+    % title(sprintf("Controller output (Nu=%d,N2=%d,lam=[%.2f,%.2f,%.2f])", ...
+    %     Nu, N2, lam(1), lam(2), lam(3)));
+    title(sprintf("Controller output (Nu=%d,N2=%d,wy=[%.2f,%.2f,%.2f])", ...
+        Nu, N2, wy(1), wy(2), wy(3)));
 
     for i = 1 : 3
         subplot(2, 2, i + 1);
