76a09abde7bba87567cff3cedef02d9c1d5f03c6
PanMij <582876465@qq.com>
2026-04-09 10:54:03 +0800

    Update the membership functions of the fuzzy controller

--- utils/build_varimp_fis.m
+++ utils/build_varimp_fis.m
@@ -9,15 +9,15 @@ function fis = build_varimp_fis()
 
     % Input 1: normalized FSI
     fis = addInput(fis, [0 1], 'Name', 'FSI_n');
-    fis = addMF(fis, 'FSI_n', 'trimf', [0 0 0.5], 'Name', 'small');
-    fis = addMF(fis, 'FSI_n', 'trimf', [0 0.5 1], 'Name', 'medium');
-    fis = addMF(fis, 'FSI_n', 'trimf', [0.5 1 1], 'Name', 'large');
+    fis = addMF(fis, 'FSI_n', 'trimf', [0 0 0.7], 'Name', 'small');
+    fis = addMF(fis, 'FSI_n', 'trimf', [0.5 0.7 1], 'Name', 'medium');
+    fis = addMF(fis, 'FSI_n', 'trimf', [0.7 1 1], 'Name', 'large');
 
     % Input 2: normalized SampEn
     fis = addInput(fis, [0 1], 'Name', 'SampEn_n');
-    fis = addMF(fis, 'SampEn_n', 'trimf', [0 0 0.5], 'Name', 'small');
-    fis = addMF(fis, 'SampEn_n', 'trimf', [0 0.5 1], 'Name', 'medium');
-    fis = addMF(fis, 'SampEn_n', 'trimf', [0.5 1 1], 'Name', 'large');
+    fis = addMF(fis, 'SampEn_n', 'trimf', [0 0 0.3], 'Name', 'small');
+    fis = addMF(fis, 'SampEn_n', 'trimf', [0 0.3 0.5], 'Name', 'medium');
+    fis = addMF(fis, 'SampEn_n', 'trimf', [0.3 1 1], 'Name', 'large');
 
     % Output: delta = DeltaZ / Zinit
     fis = addOutput(fis, [-0.03 0.01], 'Name', 'delta');
@@ -39,7 +39,7 @@ function fis = build_varimp_fis()
         1 2 1 1 1  % F small,  S medium -> NB
         1 3 1 1 1  % F small,  S large  -> NB
         2 1 3 1 1  % F medium, S small  -> ZO
-        2 2 3 1 1  % F medium, S medium -> ZO
+        2 2 2 1 1  % F medium, S medium -> NS
         2 3 2 1 1  % F medium, S large  -> NS
         3 1 4 1 1  % F large,  S small  -> PS
         3 2 3 1 1  % F large,  S medium -> ZO
