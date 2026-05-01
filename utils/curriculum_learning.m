3c3a0dc286d6a65bba4d5cb9d4a2afd59f8f0a47
PanMij <582876465@qq.com>
2026-05-01 21:34:55 +0800

    Add a new training stage

--- utils/curriculum_learning.m
+++ utils/curriculum_learning.m
@@ -1,11 +1,15 @@
 clc;clear;
 close all;
 
+useParallel = true;
+workers = 4;
+
 Ts_sup = 5;
 
 %% stage 1
-% numEpisodes = 25;
-numEpisodes = 3;
+disp("==================== Stage 1 ====================");
+
+numEpisodes = 25;
 numSteps = 10;
 
 resetParams = struct();
@@ -43,12 +47,13 @@ resetParams.reward = struct( ...
     'beta_smooth', 0.0, ...
     'beta_return', 0.0);
 
-train_vi_one_episode(numEpisodes, true, 4, numSteps, 1, resetParams);
+% train_vi_one_episode(numEpisodes, useParallel, workers, numSteps, 1, resetParams);
 
 
 %% stage 2
-% numEpisodes = 50;
-numEpisodes = 0;
+disp("==================== Stage 2 ====================");
+
+numEpisodes = 50;
 numSteps = 20;
 
 resetParams = struct();
@@ -75,4 +80,70 @@ resetParams.reward = struct( ...
     'beta_smooth', 0.0, ...
     'beta_return', 0.0);
 
-train_vi_one_episode(numEpisodes, true, 4, numSteps, 2, resetParams);
+% train_vi_one_episode(numEpisodes, useParallel, workers, numSteps, 2, resetParams);
+
+
+%% stage 3
+disp("==================== Stage 3 ====================");
+
+numEpisodes = 50;
+numSteps = 20;
+
+resetParams = struct();
+resetParams.mdl = "ctrlSys_rl_env_sg";
+resetParams.phaseIdx = 2;
+resetParams.Z_init0 = [0.055; 0.055; 0.055];
+resetParams.hy_out0 = 0.2;
+resetParams.TstopDefault = numSteps * Ts_sup;
+
+resetParams.rhoMin = 0.85;
+resetParams.rhoMax = 1.05;
+
+resetParams.ZadjScaleMin = -0.05;
+resetParams.ZadjScaleMax = 0.0;
+
+resetParams.dZ_prev_scale_max = 0.005;
+resetParams.dZ_prev_scale_min = -0.01;
+% Reward weights
+resetParams.reward = struct( ...
+    'alphaF', 1, ...
+    'alphaS', 1, ...
+    'beta_effort', 0.3, ...
+    'beta_freeze', 0.3, ...
+    'beta_smooth', 0.0, ...
+    'beta_return', 0.0);
+
+train_vi_one_episode(ceil(numEpisodes / workers), useParallel, workers, numSteps, 3, resetParams);
+
+
+%% stage 4
+disp("==================== Stage 4 ====================");
+
+numEpisodes = 50;
+numSteps = 20;
+
+resetParams = struct();
+resetParams.mdl = "ctrlSys_rl_env_sg";
+resetParams.phaseIdx = 2;
+resetParams.Z_init0 = [0.055; 0.055; 0.055];
+resetParams.hy_out0 = 0.2;
+resetParams.TstopDefault = numSteps * Ts_sup;
+
+resetParams.rhoMin = 0.95;
+resetParams.rhoMax = 1.05;
+
+resetParams.ZadjScaleMin = -0.1;
+resetParams.ZadjScaleMax = -0.05;
+
+resetParams.dZ_prev_scale_max = 0.005;
+resetParams.dZ_prev_scale_min = -0.005;
+
+resetParams.reward = struct( ...
+    'alphaF', 1, ...
+    'alphaS', 1, ...
+    'beta_effort', 0.3, ...
+    'beta_freeze', 0.3, ...
+    'beta_smooth', 0.0, ...
+    'beta_return', 1.0);
+
+train_vi_one_episode(ceil(numEpisodes / workers), useParallel, workers, numSteps, 4, resetParams);
